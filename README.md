3aa4b3d83f0bdd32f9877d86b857e8868eed9da8
Yudong Tan <ydtan@msn.com>
2015-07-12 07:22:26 -0700

    add django skeleton

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # monk
 hunting HR V1
+
+# Prerequsites
+Run script/install_unbuntu.sh to install all packages needed.
+
+# Django
+## Check Django version
+python -c "import django; print(django.get_version())"
+
+# MySQL
+## Create DB and user
+mysql -u root -p
+> CREATE DATABASE monk;
+> CREATE USER 'monk'@'localhost';
+> GRANT ALL ON monk.* TO 'monk'@'localhost';
+
--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -1,2 +1,11 @@
-sudo apt-get install git python-virtualenv
+# prerequsites
+sudo apt-get install git python-virtualenv python-mysqldb tasksel openjdk-7-jre python-pip
+
+# mysql
+sudo apt-get update
+sudo tasksel install lamp-server
+
 npm install -g ember-cli
+
+#git python-setuptools tasksel
+# openjdk-6-jre python-pip python-dev libxml2-dev libxslt-dev libjpeg-dev apache2
