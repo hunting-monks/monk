fe20ec7aba648a50fcc5d45ef35a20d22412b66f
Yudong Tan <yudong.tan@gmail.com>
2015-10-05 17:13:42 +0800

    fix a bug

--- interview_track/logic/applicationcase.py
+++ interview_track/logic/applicationcase.py
@@ -1,7 +1,8 @@
 from company.logic import employee
 from interview_track.models import ApplicationCase
 
-def get_application_cases_by_uid(uid, conditions):
+
+def get_application_cases_by_uid(uid, conditions=[]):
     """Given a user id, get all application cases that belong to the Company
     where the user is in"""
     cid = employee.get_employee_by_user(userid=uid).company.id
