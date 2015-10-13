d50ea8d7489f58435304a90be55cd47c5d729ae3
zlizen <zli@zenefits.com>
2015-10-12 21:23:34 -0700

    accounts

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,17 +5,16 @@ from django.contrib.auth.models import User
 
 LOGGER = logging.getLogger('accounts.forms')
 
-
 class CompanyRegistrationForm(forms.Form):
 	password = forms.CharField(widget=forms.PasswordInput(), label="Password")
-	email = forms.CharField(widget=forms.EmailInput(attrs={'autofocus': ''}))
+	email = forms.EmailField(widget=forms.EmailInput(attrs={'autofocus': ''}))
 	name = forms.CharField()
 	phone = forms.CharField()
 	size = forms.IntegerField(min_value=1)
 	zipCode = forms.CharField()
 	terms = forms.BooleanField(widget=forms.CheckboxInput(),
 							 label=(u'I agree to the Terms of Service'),
-							 error_messages={'required': ("You must agree to the terms to register")})
+							 error_messages={'required': ("You must agree to the terms.")})
 
 	def clean_email(self):
 		value = self.data['email']
@@ -37,7 +36,7 @@ class CompanyRegistrationForm(forms.Form):
 
 	def clean_companyZip(self):
 		if not re.match(r'\d\d\d\d\d$', self.data['zipCode']):
-			raise forms.ValidationError('Please enter a valid 5-digit zip code')
+			raise forms.ValidationError('Please enter a valid zip code')
 		return self.data['zipCode']
 
 	def clean_companyPhone(self):
@@ -53,5 +52,29 @@ class CompanyRegistrationForm(forms.Form):
 			return self.data['size']
 		except:
 			raise forms.ValidationError('Please enter a valid employee count')
+
+
+class InterviewerRegistrationForm(forms.ModelForm):
+	password1 = forms.CharField(widget=forms.PasswordInput(), label="Password")
+	password2 = forms.CharField(widget=forms.PasswordInput(), label="Repeat your password")
+	email = forms.EmailField(widget = forms.EmailInput(attrs={'autofocus': ''}))
+	terms = forms.BooleanField(widget=forms.CheckboxInput(),
+							 label=(u'I agree to the Terms of Service'),
+							 error_messages={'required': ("You must agree to the terms.")})
+
+	def clean_password1(self):
+		if self.data['password1'] != self.data['password2']:
+			raise forms.ValidationError('Passwords are not the same')
+		return self.data['password1']
+
+	def clean_email(self):
+		value = self.data['email']
+		if User.objects.filter(email=value, is_active=True).count():
+			raise forms.ValidationError("This email is already registered.")
+		return value
+
+	class Meta:
+		model = User
+		fields = ()
 
 
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,21 +1,22 @@
 from django.conf.urls import patterns, include, url
-from .forms import CompanyRegistrationForm, LongAuthenticationForm
-from .views import RegistrationView
+from django.views.generic.base import TemplateView
+from .forms import CompanyRegistrationForm, InterviewerRegistrationForm
+from .views import CompanyRegistrationView, InterviewerRegistrationView
 
 urlpatterns = patterns('',
 
 	# customize user registration form
 	url(r'^register/$',
-		RegistrationView.as_view(
-			template_name='registration/registration_form.html',
+		CompanyRegistrationView.as_view(
+			template_name='registration/registration_company_form.html',
 			form_class=CompanyRegistrationForm,
-			success_url='/dashboard/#/first'
+			success_url='/recruiter/home'
 		),
 		name='registration_register'
 	),
 	url(r'^register/done/$', 'accounts.views.register_done'),
 
-	(r'^login/$', 'accounts.views.login', {'authentication_form': LongAuthenticationForm}),
+	# (r'^login/$', 'accounts.views.login', {'authentication_form': LongAuthenticationForm}),
 
 	#(r'^logout/$', 'accounts.views.logout', {'next_page': '/'}),
 	#(r'^verify/$', 'accounts.views.verify'),
@@ -23,5 +24,46 @@ urlpatterns = patterns('',
 	#(r'^resend/$', 'accounts.views.resend_registration_email'),
 	# (r'^check-email/$', 'accounts.views.check_email'),
 
-	(r'', include('registration.backends.simple.urls'))
+	url(r'^activate/(?P<activation_key>\w+)/$',
+		InterviewerRegistrationView.as_view(
+			template_name='registration/registration_interviewer_form.html',
+			form_class=InterviewerRegistrationForm,
+			success_url='/interviewer/home'
+		),
+		name='registration_activate'
+	),
+
+	url(r'^register/closed/$',
+                           TemplateView.as_view(template_name='registration/registration_closed.html'),
+                           name='registration_disallowed'),
+
+	(r'', include('registration.backends.simple.urls')),
+
+	# url(r'^login/$',
+	#    auth_views.login,
+	#    {'template_name': 'registration/login.html'},
+	#    name='auth_login'),
+	# url(r'^logout/$',
+	#    auth_views.logout,
+	#    {'template_name': 'registration/logout.html'},
+	#    name='auth_logout'),
+	# url(r'^password/change/$',
+	#    auth_views.password_change,
+	#    {'post_change_redirect': reverse_lazy('auth_password_change_done')},
+	#    name='auth_password_change'),
+	# url(r'^password/change/done/$',
+	#    auth_views.password_change_done,
+	#    name='auth_password_change_done'),
+	# url(r'^password/reset/$',
+	#    auth_views.password_reset,
+	#    {'post_reset_redirect': reverse_lazy('auth_password_reset_done')},
+	#    name='auth_password_reset'),
+	# url(r'^password/reset/complete/$',
+	#    auth_views.password_reset_complete,
+	#    name='auth_password_reset_complete'),
+	# url(r'^password/reset/done/$',
+	#    auth_views.password_reset_done,
+	#    name='auth_password_reset_done'),
+
 )
+
