af31234736204476b43a7d4f88bdbe47bd1e31db
newtasion <newtasion@gmail.com>
2015-10-14 15:09:37 -0700

    url reverse in js

---
+++ static/js/dutils.conf.urls.js
@@ -0,0 +1,12 @@
+dutils.conf.urls = {
+    "registration_register": "/accounts/register/",
+    "auth_login": "/accounts/login/",
+    "auth_logout": "/accounts/logout/",
+
+    "home": "/",
+    "host": "/newsletter/host",
+
+    "interviewer_list": "/recruiter/list_interviewers/",
+    "add_interviewers": "/recruiter/add_interviewers/",
+    "interviewer_detail": "/recruiter/interviewer_detail/<interviewer_id>/",
+}
